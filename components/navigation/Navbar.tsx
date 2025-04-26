ce12fffd63f183bbf90b68a600a37a6c2ca0c4f8
krishpinto <161019503+krishpinto@users.noreply.github.com>
2025-04-26 21:59:59 +0530

    ek cheez reh gya commit

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -152,7 +152,7 @@ const Navbar = () => {
                             <div className="py-1">
                               <button
                                 onClick={handleLogout}
-                                className="block px-4 py-2 text-sm text-red-400 hover:bg-gray-800 w-full text-left flex items-center"
+                                className="px-4 py-2 text-sm text-red-400 hover:bg-gray-800 w-full text-left flex items-center"
                               >
                                 <LogOut className="mr-2 h-4 w-4" />
                                 <span>Logout</span>
